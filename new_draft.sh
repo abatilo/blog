9aa5e02150b18814b546ef27cff9a30c791cb216
Aaron <AaronBatilo@gmail.com>
2020-01-17 08:35:13 -0700

    Work with customers

--- new_draft.sh
+++ new_draft.sh
@@ -3,4 +3,5 @@ mkdir -p _drafts
 echo "---" > ./_drafts/new-post.md
 echo "layout: post" >> ./_drafts/new-post.md
 echo "title: New post" >> ./_drafts/new-post.md
+echo "categories: [Blog]" >> ./_drafts/new-post.md
 echo "---" >> ./_drafts/new-post.md
--- run_drafts.sh
+++ run_drafts.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -it -p4000:4000 -v`pwd`:/src -w /src jekyll/builder:3.8.5 jekyll serve --drafts --force_polling
+docker run -it -p4000:4000 -v`pwd`:/src --mount type=volume,src=bloggems,dst=/usr/local/bundle -w /src jekyll/builder:3.8.5 jekyll serve --drafts --force_polling --incremental
