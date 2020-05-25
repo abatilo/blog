76288349b88f7335c399179ee307a983bcc99b7a
Aaron <AaronBatilo@gmail.com>
2020-05-25 12:16:21 -0600

    Enable auto naming

--- index.ts
+++ index.ts
@@ -30,9 +30,7 @@ const service = deployment.createService();
 const ingressMiddleware = new k8s.apiextensions.CustomResource(appName, {
   apiVersion: "traefik.containo.us/v1alpha1",
   kind: "Middleware",
-  metadata: {
-    name: deployment.metadata.name,
-  },
+  metadata: {},
   spec: {
     headers: {
       forceSTSHeader: true,
@@ -55,9 +53,6 @@ const ingressMiddleware = new k8s.apiextensions.CustomResource(appName, {
 const ingressRoute = new k8s.apiextensions.CustomResource(appName, {
   apiVersion: "traefik.containo.us/v1alpha1",
   kind: "IngressRoute",
-  metadata: {
-    name: deployment.metadata.name,
-  },
   spec: {
     entryPoints: ["websecure"],
     routes: [
