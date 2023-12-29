f5443b16a63402e3f381136a5068dadbda796daa
Aaron Batilo <AaronBatilo@gmail.com>
2023-12-29 13:05:04 -0700

    Bump tool versions again

--- scripts/drafts.sh
+++ scripts/drafts.sh
@@ -4,4 +4,4 @@ docker run \
   --rm -it \
   -v $(pwd):/src -w /src \
   -p 1313:1313 \
-  hugomods/hugo:0.117.0 sh -c "hugo server --buildDrafts --buildFuture --navigateToChanged --bind=0.0.0.0"
+  hugomods/hugo:0.121.1 sh -c "hugo server --buildDrafts --buildFuture --navigateToChanged --bind=0.0.0.0"
